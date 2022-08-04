f50fc71da3ca096791a5c7bbe019625c812ac64a
tkc-poly-tech <50881997+tkc-poly-tech@users.noreply.github.com>
2022-08-04 19:38:35 +0900

    piece.c fixed variable scope

--- src/piece.c
+++ src/piece.c
@@ -5,11 +5,10 @@ Piece copyPiece(Piece piece)
 	Piece copied = piece;
 	char **array = piece.array;
 	copied.array = (char **)malloc(copied.width * sizeof(char *));
-	int i, j;
-	for (i = 0; i < copied.width; i++)
+	for (int i = 0; i < copied.width; i++)
 	{
 		copied.array[i] = (char *)malloc(copied.width * sizeof(char));
-		for (j = 0; j < copied.width; j++)
+		for (int j = 0; j < copied.width; j++)
 			copied.array[i][j] = array[i][j];
 	}
 	return (copied);
@@ -17,8 +16,7 @@ Piece copyPiece(Piece piece)
 
 void deletePiece(Piece piece)
 {
-	int i;
-	for (i = 0; i < piece.width; i++)
+	for (int i = 0; i < piece.width; i++)
 		free(piece.array[i]);
 	free(piece.array);
 }
@@ -26,14 +24,11 @@ void deletePiece(Piece piece)
 void rotatePiece(Piece piece) // rotates clockwise
 {
 	Piece tmp = copyPiece(piece);
-	int i, j, k, width;
-	width = piece.width;
-	for (i = 0; i < width; i++)
+	int width = piece.width;
+	for (int i = 0; i < width; i++)
 	{
-		for (j = 0, k = width - 1; j < width; j++, k--)
-		{
+		for (int j = 0, k = width - 1; j < width; j++, k--)
 			piece.array[i][j] = tmp.array[k][i];
-		}
 	}
 	deletePiece(tmp);
 }
